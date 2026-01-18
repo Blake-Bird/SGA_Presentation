27f7f5f213df64e736704c81884a6e97796f5746
Blockade279 <bbird1@highpoint.edu>
2026-01-18 12:57:53 -0500

    Switch to normal Next.js build for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Vercel deploys Next.js normally (do NOT use output: "export")
-};
+const nextConfig = {};
 
 export default nextConfig;
